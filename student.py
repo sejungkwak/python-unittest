dcd8d5c75a482b21bc7515fe2337efaba6d38306
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-26 14:38:15 +0000

    Add email method to Student class

--- student.py
+++ student.py
@@ -17,3 +17,7 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
