0b2da733a912801697d643eb6d7f294f82d41b3e
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-26 12:35:25 +0000

    Add test for empty list argument

--- test_evens.py
+++ test_evens.py
@@ -7,6 +7,9 @@ class TestEvens(unittest.TestCase):
     def test_throws_error_if_value_passed_in_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens, 4)
 
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+
 
 if __name__ == '__main__':
     unittest.main()
