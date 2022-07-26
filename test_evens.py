6fa179653b43609e2da44955ecd93030fd8e58bd
Sejung Kwak <kwak.sejung@gmail.com>
2022-07-26 12:28:56 +0000

    Add test for exception

--- test_evens.py
+++ test_evens.py
@@ -3,9 +3,10 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    
-    def test_function_returns_True(self):
-        self.assertTrue(even_number_of_evens([]))
+
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
